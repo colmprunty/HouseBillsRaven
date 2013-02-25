3a621f53edade0f7f2e3379575fbc909e2bf22be
colmprunty <colm.prunty@teamdatalogic.com>
2013-02-25 22:50:06 +0000

    Insert people

---
+++ HouseBillsRaven/HouseBillsRaven/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using HouseBillsRaven.Models;
+
+namespace HouseBillsRaven.Controllers
+{
+    public class PersonController : BaseController
+    {
+        [HttpGet]
+        public ActionResult InsertPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertPerson(Person newPerson)
+        {
+            RavenSession.Store(newPerson);
+            RavenSession.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
